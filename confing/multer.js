07c545f921a349ec25333b902ee6e90a58476f0f
SHAFINSHAMUHAMMED <122475859+SHAFINSHAMUHAMMED@users.noreply.github.com>
2023-09-15 15:29:56 +0530

    cors changed to old

--- confing/multer.js
+++ confing/multer.js
@@ -1,6 +1,7 @@
 import multer from 'multer';
 
 function creatmullter() {
+  console.log('1')
   const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, 'uploads');
@@ -9,8 +10,10 @@ function creatmullter() {
       cb(null, file.originalname);
     }
   });
-  const upload = multer({ storage: storage }) 
+  console.log('2')
 
+  const upload = multer({ storage: storage }) 
+  console.log(upload,'jjjjjjjjjjjjjjjjjjjjjj')
   return upload;
 }
 
